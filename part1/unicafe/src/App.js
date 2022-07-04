18b680dbea388bea6ef21ebfe8fbce674e99c246
Pere Garcia <peredirtbike@gmail.com>
2022-07-04 13:32:54 +0200

    Exercise 1.7 (not finished)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 
+
 const App = () => {
+
+  const goodHandleClick = () => {
+    setGood(good +1)
+    console.log(good)
+  }
+
+  const neutralHandleClick = () => {
+    setNeutral(neutral +1)
+  }
+
+  const badHandleClick = () => {
+    setBad(bad +1)
+  }
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  
   return (
     <div>
-      <p>Hola a</p>
+      <button onClick={goodHandleClick}>Good</button>
+      <p>{good}</p>
+      <button onClick={neutralHandleClick}>Neutral</button>
+      <p>{neutral}</p>
+      <button onClick={badHandleClick}>Bad</button>
+      <p>{bad}</p>
     </div>
   )
 }
