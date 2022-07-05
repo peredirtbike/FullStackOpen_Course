18e4b94d534f349659948c412e518eb2cd2e7158
Pere Garcia <peredirtbike@gmail.com>
2022-07-05 13:34:53 +0200

    Exercise 1.7 (Finished)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,31 +5,42 @@ const App = () => {
 
   const goodHandleClick = () => {
     setGood(good +1)
-    console.log(good)
+    setTotal(total +1)
   }
 
   const neutralHandleClick = () => {
     setNeutral(neutral +1)
+    setTotal(total +1)
   }
 
   const badHandleClick = () => {
     setBad(bad +1)
+    setTotal(total +1)
   }
+  
+
 
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   
   return (
     <div>
-      <button onClick={goodHandleClick}>Good</button>
-      <p>{good}</p>
-      <button onClick={neutralHandleClick}>Neutral</button>
-      <p>{neutral}</p>
-      <button onClick={badHandleClick}>Bad</button>
-      <p>{bad}</p>
+      <h1>Give Feedback</h1>
+        <button onClick={goodHandleClick}>Good</button>
+        <button onClick={neutralHandleClick}>Neutral</button>
+        <button onClick={badHandleClick}>Bad</button>
+      <h1>Statistics</h1>
+
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {((1 * good + 0 * neutral + -1* bad)/total)}</p>
+      <p>Positive: {good / total * 100} %</p>
     </div>
   )
 }
