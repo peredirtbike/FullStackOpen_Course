b842822a0c68e16f5c01e0cd71bb9a88e59f8523
Pere Garcia <peredirtbike@gmail.com>
2022-07-06 12:01:47 +0200

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,46 +1,79 @@
 import { useState } from 'react'
 
+const Heading = ({title}) => <h1>{title}</h1>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Statistics = (props) => {
+  if (props.total === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  } else{
+    return (
+      <div>
+        <StatisticsLine text="Good" value={props.feedbackCategory.good} />
+        <StatisticsLine text="Neutral" value={props.feedbackCategory.neutral} />
+        <StatisticsLine text="Bad" value={props.feedbackCategory.bad} />
+        <StatisticsLine text="Total" value={props.total} />
+        <StatisticsLine text="Average" value={props.average} />
+        <StatisticsLine text="Positive Percentage" value={props.positivePercentage} endDelimiter='%' />
+      </div>
+    )
+  }
+}
+
+const StatisticsLine = ({text, value, endDelimiter=''}) => {
+  return(
+  <div>
+    <p>{text} {value} {endDelimiter}</p>
+  </div>
+  )
+}
 
 const App = () => {
 
-  const goodHandleClick = () => {
-    setGood(good +1)
-    setTotal(total +1)
+  const totalFeedback = () => good + neutral + bad
+
+  const averageFeedback = () => (good * scores.good) + (neutral * scores.neutral) + (bad * scores.bad) / totalFeedback()
+
+  const positiveFeedbackPercentage = () => {
+    let percentage = (good / totalFeedback()) * 100
+    return percentage.toFixed(1)
   }
 
-  const neutralHandleClick = () => {
-    setNeutral(neutral +1)
-    setTotal(total +1)
-  }
+  const setFeedback = (feedbackCategory, changeStateFunc) => (
+    changeStateFunc(feedbackCategory +1)
+  ) 
 
-  const badHandleClick = () => {
-    setBad(bad +1)
-    setTotal(total +1)
-  }
   
-
-
+  
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-
-  
+  const scores = {
+    good: 1,
+    neutral: 0,
+    bad: -1
+  }
   return (
     <div>
-      <h1>Give Feedback</h1>
-        <button onClick={goodHandleClick}>Good</button>
-        <button onClick={neutralHandleClick}>Neutral</button>
-        <button onClick={badHandleClick}>Bad</button>
-      <h1>Statistics</h1>
-
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Average: {((1 * good + 0 * neutral + -1* bad)/total)}</p>
-      <p>Positive: {good / total * 100} %</p>
+      <Heading title="Give feedback" />
+        <Button handleClick={() => setFeedback(good, setGood)} text="good" />
+        <Button handleClick={() => setFeedback(neutral, setNeutral)} text="neutral" />
+        <Button handleClick={() => setFeedback(bad, setBad)} text="bad" />
+      <Heading title="Statistics" />
+      <Statistics 
+        feedbackCategory={{good,neutral,bad}}
+        total={totalFeedback()}
+        average={averageFeedback()}
+        positivePercentage={positiveFeedbackPercentage()}
+      />
     </div>
   )
 }
