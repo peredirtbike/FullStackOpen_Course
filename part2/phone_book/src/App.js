4fa49c4d81360c1ddae112ff9afd922f1f034e54
Pere Garcia <peredirtbike@gmail.com>
2022-07-26 13:30:03 +0200

    Part 2.9

--- part2/phone_book/src/App.js
+++ part2/phone_book/src/App.js
@@ -2,46 +2,67 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Pere Garcia'}
+    { 
+      name: 'Arto Hellas', number: '644312455' 
+    },
+    { 
+      name: 'Pere Garcia', number: '561232145'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
-  const renderListOfUserNames = (names, index) => {
-    return names.map(({name}) => <li key={index}>{name}</li>)
+
+  const renderObject = (objects, index) => {
+    return objects.map((object, index) => <li key={index}>{object.name} : {object.number}</li>)
+  }
+
+  const filtered = persons.filter(person => {
+    return person.name.includes(newFilter);
+  });
+
+  const renderFilteredObject = (objects, index) => {
+    return objects.map((object, index) => <li key={index}>{object.name} : {object.number}</li>)
   }
 
   const addName = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
     const nameObject = {
-      name : newName
+      name : newName,
+      number: newNumber
     }
-    console.log(nameObject.name)
-    // if (nameObject.name in persons.name){
-    //   console.log("este nombre ya está")
-    // }
-    const isInArray = persons.filter(person => {
-      if(person.name === nameObject.name){
-        console.log("Está en la lista")
-        setPersons('')
-      } return true
+    const isInArray = persons.find(person => {
+      if(JSON.stringify(person.name) === JSON.stringify(nameObject.name)){
+        console.log(nameObject.name, "está en la lista, por lo tanto no se añade")
+          setPersons(persons)
+          setNewName('')
+          setNewNumber('')
+          return true
+      } else{
+        console.log("se añade")
+        setPersons(persons.concat(nameObject))
+        setNewName('')
+        setNewNumber('')
+        alert(nameObject.name + " is already added to phonebook")
+        return false
+      }
     })
-
-    if (isInArray === true) {
-
-    }else{
-      console.log(persons)
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-    }
    
-
   }
   const handleNameChange = (event) => {
     event.preventDefault()
-    console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    event.preventDefault()
+    setNewFilter(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    event.preventDefault()
+    setNewNumber(event.target.value)
   }
 
 
@@ -51,19 +72,23 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          <h2>Phone Book</h2>
+          filter shown with: <input value={newFilter} onChange={handleFilterChange} /> <br />
+
+        </div>
+        <div>
+          <h2>Add a new:</h2>
+          name: <input value={newName} onChange={handleNameChange} /> <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <div>
-          <ul>
-            {renderListOfUserNames(persons)}
-          </ul>
-        </div>
       <h2>Numbers</h2>
-      ...
+          <ul>
+            {renderFilteredObject(filtered)}
+          </ul>
     </div>
   )
 }
