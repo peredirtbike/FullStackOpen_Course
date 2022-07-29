fe023526e1bd6b75b7920f334d32b30e1b248847
Pere Garcia <peredirtbike@gmail.com>
2022-07-29 13:48:57 +0200

    Part 2.14

--- part2/countries_api/src/App.js
+++ part2/countries_api/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+
+
+
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+  const [searchCountry, setSearchCountry] = useState('')
+
+    useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        const data = response.data
+        setCountries(data)
+      })
+  }, [])
+
+  
+  const handleCountryChange = (event) => {
+    console.log(event.target.value)
+    setSearchCountry(event.target.value)
+  }
+
+  const filter = countries.filter(object => {
+    return object.name.common.toLowerCase().startsWith(searchCountry.toLowerCase())
+  })
+
+  console.log(filter)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries <input value={searchCountry} onChange={handleCountryChange}></input>
+      { <ul>
+        {filter.map((filter, index)=>
+        <div>
+          <li key={index+1}>{filter.name.common}</li>
+          <img alt='tete' key={index} src={filter.flags.png}></img>
+        </div>
+        )}
+      </ul> }
+
     </div>
   );
 }
